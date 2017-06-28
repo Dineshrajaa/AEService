8518534c214c8b74d0df6a4abaaf1fb0fe821ed8
Dinesh Raja <dineshrajaa.93@gmail.com>
2017-06-28 12:53:59 +0530

    Add Organization base setup

--- controllers/UserAccount.controller.js
+++ controllers/UserAccount.controller.js
@@ -1,87 +1,131 @@
-var config         = require('../config'),
+var config = require('../config'),
 	UserAccount = require('../models/UserAccount.model'),
 	UserAccountServices = require('../services/UserAccount.service'),
 	helperServices = require('../services/helper.service'),
 	moment = require('moment'),
 	fs = require("fs"),
-	Promise        = require("bluebird"),
+	Promise = require("bluebird"),
 	orm = require('../orm');
 /*Get all categories*/
-exports.registerUser = function(req, res){
+exports.registerUser = function (req, res) {
 	console.log("registerUser");
-	password = helperServices.encryption(req.body.Password,req.body.EmailId);
-		req.body.Password = password;
-		UserAccountServices.getUserByEmail(req.body.EmailId).then(function(result){
+	password = helperServices.encryption(req.body.Password, req.body.EmailId);
+	req.body.Password = password;
+	UserAccountServices.getUserByEmail(req.body.EmailId).then(function (result) {
 
-		if(result==0){
-			req.body.Fullname = req.body.FirstName+' '+req.body.LastName;
-			req.body.ModifyDate =moment().format('YYYY-MM-DD HH:mm:ss');
-			req.body.CreateDate =moment().format('YYYY-MM-DD HH:mm:ss');
-			return UserAccountServices.registerUser(req.body).then(function(result1){
-				res.json({"StatusCode":200,"user":result1,"ResponseMessage":"New user created successfully!"});
-			}).catch(function(err){
-				res.json({"StatusCode":err.status,"user":[],"ResponseMessage":err.messages});
+		if (result == 0) {
+			if (req.body.Role === 'business') {
+				// This is a business app request so register organization and save it to request
+				UserAccountServices.registerOrganization(req.body).then(function (result) {
+					console.warn(result);
+					// req.body.OrgId=result.
+				});
+			}
+			req.body.Fullname = req.body.FirstName + ' ' + req.body.LastName;
+			req.body.ModifyDate = moment().format('YYYY-MM-DD HH:mm:ss');
+			req.body.CreateDate = moment().format('YYYY-MM-DD HH:mm:ss');
+			return UserAccountServices.registerUser(req.body).then(function (result1) {
+				res.json({
+					"StatusCode": 200,
+					"user": result1,
+					"ResponseMessage": "New user created successfully!"
+				});
+			}).catch(function (err) {
+				res.json({
+					"StatusCode": err.status,
+					"user": [],
+					"ResponseMessage": err.messages
+				});
 			});
-			
-		}else{
-			res.json({"StatusCode":302,"user":[],"ResponseMessage":"User Alredy Exist!!"});
+
+		} else {
+			res.json({
+				"StatusCode": 302,
+				"user": [],
+				"ResponseMessage": "User Alredy Exist!!"
+			});
 		}
-		
-	}).catch(function(err){
-		res.json({"StatusCode":err.status,"Favourites":[],"ResponseMessage":err.messages});
+
+	}).catch(function (err) {
+		res.json({
+			"StatusCode": err.status,
+			"Favourites": [],
+			"ResponseMessage": err.messages
+		});
 	});
 }
 
 //get all users
-exports.getAllUsers = function(req, res){
+exports.getAllUsers = function (req, res) {
 	console.log("getAllUsers");
-	UserAccountServices.getAllUsers().then(function(result){
+	UserAccountServices.getAllUsers().then(function (result) {
 		res.json(result);
-	}).catch(function(err){
-		res.json({"StatusCode":err.status,"Favourites":[],"ResponseMessage":err.messages});
+	}).catch(function (err) {
+		res.json({
+			"StatusCode": err.status,
+			"Favourites": [],
+			"ResponseMessage": err.messages
+		});
 	});
 }
 
 //get user by id
-exports.GetUserAccount = function(req, res){
+exports.GetUserAccount = function (req, res) {
 	console.log("GetUserAccount");
-	var UserId = (req.query.id)?req.query.id:false;
-	UserAccountServices.GetUserAccount(UserId).then(function(result){
+	var UserId = (req.query.id) ? req.query.id : false;
+	UserAccountServices.GetUserAccount(UserId).then(function (result) {
 
-		if(!result)
-			res.json({"Message": "An error has occurred."});
+		if (!result)
+			res.json({
+				"Message": "An error has occurred."
+			});
 		else
 			res.json(result);
-	}).catch(function(err){
-		res.json({"StatusCode":err.status,"Favourites":[],"ResponseMessage":err.messages});
+	}).catch(function (err) {
+		res.json({
+			"StatusCode": err.status,
+			"Favourites": [],
+			"ResponseMessage": err.messages
+		});
 	});
 }
 
 //get user by id with proper response
-exports.GetUserAccountById = function(req, res){
+exports.GetUserAccountById = function (req, res) {
 	console.log("GetUserAccountById");
-	var UserId = (req.query.id)?req.query.id:false;
+	var UserId = (req.query.id) ? req.query.id : false;
 	console.log(UserId);
-	if(UserId){
-			UserAccountServices.GetUserAccount(UserId).then(function(result){
-				console.log(result.get("DOB"));
-				if(!result){
-					return res.json({"Message": "An error has occurred."});
-				}
-				else{
-					
-						result.set("UserImage",config.image_url+result.get("UserImage"));
-						return result;
-				}
-			}).then(function(fresult){
-					res.json({"StatusCode": 200,"userAccount":fresult,"ResponseMessage": "Wow!! you are so passionate about your UserAccountRepository"});
-			}).catch(function(err){
-					res.json({"StatusCode":err.status,"Favourites":[],"ResponseMessage":err.messages});
+	if (UserId) {
+		UserAccountServices.GetUserAccount(UserId).then(function (result) {
+			console.log(result.get("DOB"));
+			if (!result) {
+				return res.json({
+					"Message": "An error has occurred."
+				});
+			} else {
+
+				result.set("UserImage", config.image_url + result.get("UserImage"));
+				return result;
+			}
+		}).then(function (fresult) {
+			res.json({
+				"StatusCode": 200,
+				"userAccount": fresult,
+				"ResponseMessage": "Wow!! you are so passionate about your UserAccountRepository"
 			});
-	}else{
-		res.json({"Message": "No HTTP resource was found that matches the request URI '"+config.webUri+"/Aesthetic/api/Users/GetById'."});
+		}).catch(function (err) {
+			res.json({
+				"StatusCode": err.status,
+				"Favourites": [],
+				"ResponseMessage": err.messages
+			});
+		});
+	} else {
+		res.json({
+			"Message": "No HTTP resource was found that matches the request URI '" + config.webUri + "/Aesthetic/api/Users/GetById'."
+		});
 	}
-	
+
 }
 
 //update user profile
@@ -96,85 +140,106 @@ exports.GetUserAccountById = function(req, res){
     user.PostCode = userAccount.PostCode;
     user.Profile = userAccount.Profile;
 }*/
-exports.updateUserProfile = function(req, res){
+exports.updateUserProfile = function (req, res) {
 	console.log("updateUserProfile");
-	if(req.body.UserImage){
-		var min = 100000; 
-	    var max = 999999; 
-	    var name = Math.floor(Math.random() * (max - min + 1)) + min+".png";
-	    var filename=config.image_path+name;
-	    var path = "Upload/User/"+name;
-	    fs.writeFile(filename, new Buffer(req.body.UserImage, "base64"),function(err){
-	      console.log(err);
-	      console.log(filename);
-	    });
+	if (req.body.UserImage) {
+		var min = 100000;
+		var max = 999999;
+		var name = Math.floor(Math.random() * (max - min + 1)) + min + ".png";
+		var filename = config.image_path + name;
+		var path = "Upload/User/" + name;
+		fs.writeFile(filename, new Buffer(req.body.UserImage, "base64"), function (err) {
+			console.log(err);
+			console.log(filename);
+		});
 
 		req.body.UserImage = path;
 	}
-  	
-	return orm.bookshelf.transaction(function(trx) {
-    	return UserAccountServices.GetUserAccount(req.body.UserId,trx)
-      	.then(function(user){
-      		if(user)
-          		return UserAccountServices.UpdateUserAccount(req.body, trx);
-          	else 
-          		return false;        
-      	})
-    	.catch(function(err) {
-      		console.log(err);
-    		}); 
-    })  
-    .then(function(results){
-      	if(!results)
-			res.json({"StatusCode": 417,"ResponseMessage": "Object reference not set to an instance of an object."});
-		else
-			res.json({"StatusCode": 200,"ResponseMessage": "Updated Successfully!!!"});
-    })
-    .catch(function(err) {
-      res.json({"StatusCode":err.status,"Favourites":[],"ResponseMessage":err.messages});
-    }); 
+
+	return orm.bookshelf.transaction(function (trx) {
+			return UserAccountServices.GetUserAccount(req.body.UserId, trx)
+				.then(function (user) {
+					if (user)
+						return UserAccountServices.UpdateUserAccount(req.body, trx);
+					else
+						return false;
+				})
+				.catch(function (err) {
+					console.log(err);
+				});
+		})
+		.then(function (results) {
+			if (!results)
+				res.json({
+					"StatusCode": 417,
+					"ResponseMessage": "Object reference not set to an instance of an object."
+				});
+			else
+				res.json({
+					"StatusCode": 200,
+					"ResponseMessage": "Updated Successfully!!!"
+				});
+		})
+		.catch(function (err) {
+			res.json({
+				"StatusCode": err.status,
+				"Favourites": [],
+				"ResponseMessage": err.messages
+			});
+		});
 }
 
-exports.GetUsersImageById = function(req, res){
+exports.GetUsersImageById = function (req, res) {
 	console.log("GetUsersImageById");
-	var UserId = (req.query.Id)?req.query.Id:false;
-	if(UserId){
-		UserAccountServices.GetUsersImageById(UserId).then(function(result){
-			if(!result.length){
+	var UserId = (req.query.Id) ? req.query.Id : false;
+	if (UserId) {
+		UserAccountServices.GetUsersImageById(UserId).then(function (result) {
+			if (!result.length) {
 				return [];
-			}
-			else{
-				return Promise.map(result.models,function(user){
-					user.set("UserImage",config.image_url+user.get("UserImage"));
+			} else {
+				return Promise.map(result.models, function (user) {
+					user.set("UserImage", config.image_url + user.get("UserImage"));
 					return user;
 				})
-				
+
 			}
-		}).then(function(fresult){
+		}).then(function (fresult) {
 			console.log(fresult);
-			if(fresult.length){
-				res.json({"StatusCode": 200,"lstUser":fresult,"ResponseMessage": "Wow!! you are so passionate about your UserImageRepository"});
-			}else{
-				res.json({"Message": "An error has occurred."});
+			if (fresult.length) {
+				res.json({
+					"StatusCode": 200,
+					"lstUser": fresult,
+					"ResponseMessage": "Wow!! you are so passionate about your UserImageRepository"
+				});
+			} else {
+				res.json({
+					"Message": "An error has occurred."
+				});
 			}
 
-		}).catch(function(err){
-			res.json({"StatusCode":err.status,"lstUser":[],"ResponseMessage":err.messages});
+		}).catch(function (err) {
+			res.json({
+				"StatusCode": err.status,
+				"lstUser": [],
+				"ResponseMessage": err.messages
+			});
 		});
-	}else{
-		res.json({"Message": "No HTTP resource was found that matches the request URI '"+config.webUri+"/Aesthetic/api/Users/GetUsersImageById'."});
+	} else {
+		res.json({
+			"Message": "No HTTP resource was found that matches the request URI '" + config.webUri + "/Aesthetic/api/Users/GetUsersImageById'."
+		});
 	}
 }
 
-exports.ChangePassword = function(req, res){
+exports.ChangePassword = function (req, res) {
 	console.log("ChangePassword");
 	console.log(req.body);
-	var data1 ={
-		"oldPassword":(req.body.oldPassword)?req.body.oldPassword:false,
-		"Newpassword":(req.body.Newpassword)?req.body.Newpassword:false,
-		"UserId":(req.body.UserId)?req.body.UserId:false,
+	var data1 = {
+		"oldPassword": (req.body.oldPassword) ? req.body.oldPassword : false,
+		"Newpassword": (req.body.Newpassword) ? req.body.Newpassword : false,
+		"UserId": (req.body.UserId) ? req.body.UserId : false,
 	}
-	if(!data1.UserId || !data1.oldPassword || !data1.Newpassword){
+	if (!data1.UserId || !data1.oldPassword || !data1.Newpassword) {
 		/*return UserAccountServices.ChangePassword(data).then(function(result){
 			if(result)
 				res.json({"StatusCode": 200,"ResponseMessage": "Updated Successfully!!!"});
@@ -184,48 +249,92 @@ exports.ChangePassword = function(req, res){
 			res.json({"StatusCode":err.status,"Favourites":[],"ResponseMessage":err.messages});
 		})*/
 
-		res.json({"StatusCode": 417,"ResponseMessage": "Object reference not set to an instance of an object."});
-	
-	
+		res.json({
+			"StatusCode": 417,
+			"ResponseMessage": "Object reference not set to an instance of an object."
+		});
 
 
-	}else{
-return UserAccountServices.GetUserAccount(data1.UserId).then(function(result){
-		if(!result){
-			res.json({"Message": "An error has occurred."});
-		}
-		else{
-			password = helperServices.encryption(data1.oldPassword,result.get('EmailId'));
-			if(password == result.get('Password')){
-				newpassword = helperServices.encryption(data1.Newpassword,result.get('EmailId'));
-				var data ={};
-				data.Password = newpassword;
-				data.oldpassword = password;
-				data.Newpassword = newpassword;
-				data.UserId = data1.UserId;
-				return UserAccountServices.UpdateUserAccount(data).then(function(result){
 
-					if(result){
-						res.json({"StatusCode": 200,"ResponseMessage": "Updated Successfully!!!"});
-					}
-					else{
-						res.json({"StatusCode": 417,"ResponseMessage": "Object reference not set to an instance of an object."});
-					}
-				}).catch(function(){
-					res.json({"StatusCode":err.status,"Favourites":[],"ResponseMessage":err.messages});
-				})
-			}else{
-				res.json({"StatusCode": 401,"userAccount": {"UserId": 149,"UserImage": null,"FirstName": null,"LastName": null,"Password": null,"ConfirmPassword": null,"EmailId": null,"ConfirmEmail": null,"Gender": null,"Address": null,"Town": null, "City": null,"PostCode": 0,"Profile": null,"TagLine": null,"DOB": null,"Role": null,"IsActive": null,"CreateDate": null,"ModifyDate": null,"oldpassword": "12345688888","Newpassword": "1234567","Fullname": null},"ResponseMessage": "Invalid Password"});
+
+	} else {
+		return UserAccountServices.GetUserAccount(data1.UserId).then(function (result) {
+			if (!result) {
+				res.json({
+					"Message": "An error has occurred."
+				});
+			} else {
+				password = helperServices.encryption(data1.oldPassword, result.get('EmailId'));
+				if (password == result.get('Password')) {
+					newpassword = helperServices.encryption(data1.Newpassword, result.get('EmailId'));
+					var data = {};
+					data.Password = newpassword;
+					data.oldpassword = password;
+					data.Newpassword = newpassword;
+					data.UserId = data1.UserId;
+					return UserAccountServices.UpdateUserAccount(data).then(function (result) {
+
+						if (result) {
+							res.json({
+								"StatusCode": 200,
+								"ResponseMessage": "Updated Successfully!!!"
+							});
+						} else {
+							res.json({
+								"StatusCode": 417,
+								"ResponseMessage": "Object reference not set to an instance of an object."
+							});
+						}
+					}).catch(function () {
+						res.json({
+							"StatusCode": err.status,
+							"Favourites": [],
+							"ResponseMessage": err.messages
+						});
+					})
+				} else {
+					res.json({
+						"StatusCode": 401,
+						"userAccount": {
+							"UserId": 149,
+							"UserImage": null,
+							"FirstName": null,
+							"LastName": null,
+							"Password": null,
+							"ConfirmPassword": null,
+							"EmailId": null,
+							"ConfirmEmail": null,
+							"Gender": null,
+							"Address": null,
+							"Town": null,
+							"City": null,
+							"PostCode": 0,
+							"Profile": null,
+							"TagLine": null,
+							"DOB": null,
+							"Role": null,
+							"IsActive": null,
+							"CreateDate": null,
+							"ModifyDate": null,
+							"oldpassword": "12345688888",
+							"Newpassword": "1234567",
+							"Fullname": null
+						},
+						"ResponseMessage": "Invalid Password"
+					});
+				}
 			}
-		}
-	}).catch(function(err){
-		res.json({"StatusCode":err.status,"ResponseMessage":err.messages});
-	});
+		}).catch(function (err) {
+			res.json({
+				"StatusCode": err.status,
+				"ResponseMessage": err.messages
+			});
+		});
 
 
 
 
 
 
-		}
+	}
 }
--- services/UserAccount.service.js
+++ services/UserAccount.service.js
@@ -1,83 +1,105 @@
 var UserAccount = require('../models/UserAccount.model'),
-    moment = require('moment');
-    
-exports.registerUser = function(params) {
-    var User = new UserAccount({
-                FirstName: (params.FirstName)?params.FirstName:null, 
-                LastName: (params.LastName)?params.LastName:null, 
-                Password: (params.Password)?params.Password:null,
-                ConfirmPassword: (params.ConfirmPassword)?params.ConfirmPassword:null,
-                EmailId: (params.EmailId)?params.EmailId:null,
-                // ConfirmEmail:(params.EmailId)?params.EmailId:null,
-                Role: (params.Role)?params.Role:null,
-                UserImage: 'upload/user/UserDefault.png',
-                ConfirmEmail: (params.ConfirmEmail)?params.ConfirmEmail:null,
-                Gender: (params.Gender)?params.Gender:null,
-                Address: (params.Address)?params.Address:null,
-                Town: (params.Town)?params.Town:null,
-                City: (params.City)?params.City:null,
-                PostCode: (params.PostCode)?params.PostCode:0,
-                Profile: (params.Profile)?params.Profile:null,
-                TagLine: (params.TagLine)?params.TagLine:null,
-                DOB: (params.DOB)?params.DOB:null,
-                IsActive: (params.IsActive)?params.IsActive:null,
-                CreateDate: (params.CreateDate)?params.CreateDate:new Date(),
-                ModifyDate: (params.ModifyDate)?params.ModifyDate:new Date(),
-                // oldpassword:(params.oldpassword)?params.oldpassword:null,
-                // Newpassword:(params.Newpassword)?params.Newpassword:null,
-                // Fullname:(params.Fullname)?params.Fullname:null,
-                // Added for Pro version
-                RegistrationNumber:(params.RegistrationNumber)?params.RegistrationNumber:null,
-                Profession:(params.Profession)?params.Profession:null
-              });
-    return User.save(null).tap(function (model){
-      userData = model;
-      return userData;
-    }).then(function(userData){
-      return userData;
-    }).catch(function(err){
-      return err;
-    });
- };
+  Organization = require('../models/Organization.model'),
+  moment = require('moment');
+
+exports.registerUser = function (params) {
+  var User = new UserAccount({
+    FirstName: (params.FirstName) ? params.FirstName : null,
+    LastName: (params.LastName) ? params.LastName : null,
+    Password: (params.Password) ? params.Password : null,
+    ConfirmPassword: (params.ConfirmPassword) ? params.ConfirmPassword : null,
+    EmailId: (params.EmailId) ? params.EmailId : null,
+    // ConfirmEmail:(params.EmailId)?params.EmailId:null,
+    Role: (params.Role) ? params.Role : null,
+    UserImage: 'upload/user/UserDefault.png',
+    ConfirmEmail: (params.ConfirmEmail) ? params.ConfirmEmail : null,
+    Gender: (params.Gender) ? params.Gender : null,
+    Address: (params.Address) ? params.Address : null,
+    Town: (params.Town) ? params.Town : null,
+    City: (params.City) ? params.City : null,
+    PostCode: (params.PostCode) ? params.PostCode : 0,
+    Profile: (params.Profile) ? params.Profile : null,
+    TagLine: (params.TagLine) ? params.TagLine : null,
+    DOB: (params.DOB) ? params.DOB : null,
+    IsActive: (params.IsActive) ? params.IsActive : null,
+    CreateDate: (params.CreateDate) ? params.CreateDate : new Date(),
+    ModifyDate: (params.ModifyDate) ? params.ModifyDate : new Date(),
+    // oldpassword:(params.oldpassword)?params.oldpassword:null,
+    // Newpassword:(params.Newpassword)?params.Newpassword:null,
+    // Fullname:(params.Fullname)?params.Fullname:null,
+    // Added for Pro version
+    RegistrationNumber: (params.RegistrationNumber) ? params.RegistrationNumber : null,
+    Profession: (params.Profession) ? params.Profession : null
+  });
+  return User.save(null).tap(function (model) {
+    userData = model;
+    return userData;
+  }).then(function (userData) {
+    return userData;
+  }).catch(function (err) {
+    return err;
+  });
+};
+// register organization
+exports.registerOrganization = function (params) {
+  var Organization = new Organization({
+    RegistrationNo: params.RegistrationNo,
+    Profession: params.Profession
+  });
+  return Organization.save(null).tap(function (model) {
+    organizationData = model;
+    return organizationData;
+  }).then(function (organizationData) {
+    return organizationData;
+  }).catch(function (err) {
+    return err;
+  });
+};
 //check a user is existed or not
-exports.getUserByEmail = function(email){
+exports.getUserByEmail = function (email) {
   return UserAccount.forge().query(function (qb) {
-      qb.where({'EmailId' : email});
-  }).fetch().then(function(User) {
-      if(User)
-        return User.get('UserId');
-      else
-        return 0;
+    qb.where({
+      'EmailId': email
+    });
+  }).fetch().then(function (User) {
+    if (User)
+      return User.get('UserId');
+    else
+      return 0;
   });
 }
 //get list of all users
-exports.getAllUsers = function(){
+exports.getAllUsers = function () {
   var fetchParams = {};
   return UserAccount.forge().fetchAll(fetchParams);
 }
 
 //get user detail using userId
-exports.GetUserAccount = function(UserId){
+exports.GetUserAccount = function (UserId) {
   var fetchParams = {};
-  return UserAccount.forge().query(function(qb){
-    if(UserId)
-      qb.where({'UserId':UserId});
+  return UserAccount.forge().query(function (qb) {
+    if (UserId)
+      qb.where({
+        'UserId': UserId
+      });
   }).fetch(fetchParams);
 }
 //get user image using userId
-exports.GetUsersImageById = function(UserId){
+exports.GetUsersImageById = function (UserId) {
   var fetchParams = {};
-  return UserAccount.forge().query(function(qb){
-    if(UserId)
-      qb.where({'UserId':UserId});
+  return UserAccount.forge().query(function (qb) {
+    if (UserId)
+      qb.where({
+        'UserId': UserId
+      });
   }).fetchAll(fetchParams);
 }
 //update user
-exports.UpdateUserAccount = function(params,transaction){
+exports.UpdateUserAccount = function (params, transaction) {
   console.log("UpdateUserAccount");
-  var UserId = (params.UserId)?params.UserId:false;
+  var UserId = (params.UserId) ? params.UserId : false;
   var authUpdateParams = {
-    patch:true
+    patch: true
   };
   var authFetchParams = {};
 
@@ -85,30 +107,33 @@ exports.UpdateUserAccount = function(params,transaction){
     authUpdateParams.transacting = transaction;
     authFetchParams.transacting = transaction;
   }
- /* if(params.Fullname || params.Fullname=='' || params.Fullname==null)
-    params.remove('Fullname');*/
+  /* if(params.Fullname || params.Fullname=='' || params.Fullname==null)
+     params.remove('Fullname');*/
   //console.log(params);
-  var data = { 
-  
-  "FirstName": params.FirstName,
-  "LastName": params.LastName,
-  "Gender": params.Gender,
-  "Address": params.Address,
-  "Town": params.Town,
-  "PostCode": params.PostCode,
-  "Profile": params.Profile,
-  "DOB": params.DOB };
-  if(params.UserImage)
-  data.UserImage = params.UserImage;
+  var data = {
 
-  if(params.DOB)
-    data.DOB =moment(params.DOB).format('YYYY-MM-DD HH:mm:ss');
+    "FirstName": params.FirstName,
+    "LastName": params.LastName,
+    "Gender": params.Gender,
+    "Address": params.Address,
+    "Town": params.Town,
+    "PostCode": params.PostCode,
+    "Profile": params.Profile,
+    "DOB": params.DOB
+  };
+  if (params.UserImage)
+    data.UserImage = params.UserImage;
+
+  if (params.DOB)
+    data.DOB = moment(params.DOB).format('YYYY-MM-DD HH:mm:ss');
 
   console.log(data);
-  return UserAccount.forge().query(function(qb){
-      if(UserId)
-      qb.where({'UserId':UserId});
-  }).fetch().then(function(fUser) {
-      return fUser.save(data, authUpdateParams);
+  return UserAccount.forge().query(function (qb) {
+    if (UserId)
+      qb.where({
+        'UserId': UserId
+      });
+  }).fetch().then(function (fUser) {
+    return fUser.save(data, authUpdateParams);
   });
 }
