6072205c61958c8836fe2774e9be18d2378126ba
Dinesh Raja <dineshrajaa.93@gmail.com>
2017-07-06 13:21:42 +0530

    Added followers data

--- services/Followers.service.js
+++ services/Followers.service.js
@@ -1,16 +1,25 @@
 var config = require('../config'),
     Followers = require('../models/Followers.model'),
     Organization = require('../models/Organization.model'),
-    User = require('../models/UserAccount.model'),
+    UserAccount = require('../models/UserAccount.model'),
     helperServices = require('../services/helper.service');
 
 exports.GetAllFollowers = function (value, field) {
     return Followers.forge().query(function (qb) {
-        if (field)
+        qb.select('UserAccount.FirstName', 'UserAccount.LastName', 'UserAccount.DOB',
+        'Favourite.FavouriteId','Favourite.OrgId','Favourite.UserId');
+        qb.join('UserAccount', function () {
+            this.on('Favourite.UserId', '=', 'UserAccount.UserId')
+        })
+        if (field) {
+            field = 'Favourite.' + field;
             qb.where(field, value);
+        }
+
     }).fetchAll().then(function (result) {
         return result;
     }).catch(function (err) {
+        console.log(err);
         res.json({
             "StatusCode": err.status,
             "Followers": [],
