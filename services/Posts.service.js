a2bff048def31a5028baa003e347227a38e423cc
Dinesh Raja <dineshrajaa.93@gmail.com>
2017-08-24 19:00:40 +0530

    Bug fix

--- services/Posts.service.js
+++ services/Posts.service.js
@@ -17,7 +17,7 @@ exports.GetAllPosts = function (postReqData) {
     return PostGet.forge().query(function (qb) {
         qb.select('UserAccount.FirstName', 'UserAccount.LastName', 'UserAccount.UserImage',
             'PostGet.PostId', 'PostGet.PostMessage', 'PostGet.PostTime', 'PostGet.PostImage', 'PostGet.CreateDate', 'PostGet.ModifyDate',
-            , 'Organization.OrgName', 'Organization.OrgImage', 'Organization.OrgAddress'
+            'Organization.OrgName', 'Organization.OrgImage', 'Organization.OrgAddress'
         )
         qb.join('UserAccount', function () {
             this.on('PostGet.OrgId', '=', 'UserAccount.OrgId')
